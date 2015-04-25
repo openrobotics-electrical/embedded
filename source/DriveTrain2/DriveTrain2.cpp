242975f3d9eb89cf85a0604e8f4e4d9e61934ef7
maxlazarus <max.prokopenko@gmail.com>
2015-04-25 14:15:59 -0700

    preparing for rewriting

---
+++ source/DriveTrain2/DriveTrain2.cpp
@@ -0,0 +1,17 @@
+/*
+ * DriveTrain2.cpp
+ *
+ * Created: 4/25/2015 2:09:40 PM
+ *  Author: Maxim
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
