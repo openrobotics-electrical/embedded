0685cf5badd4db617b04ae5fe30bcd654eedf0e5
maxlazarus <max.prokopenko@gmail.com>
2015-08-01 15:43:43 -0700

    added ROS to ultrasound module, sucks

--- projects/ultrasound/ultrasound.ino
+++ projects/ultrasound/ultrasound.ino
@@ -1,3 +1,5 @@
+// max.prokopenko@gmail.com
+
 #define TRIG_1 9
 #define TRIG_2 7
 #define TRIG_3 5
@@ -9,9 +11,34 @@
 #define BOARD_LED 13
 #define TIMEOUT_uS 60600 // 10m both ways @ 330m/s sound velocity
 
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/Range.h>
+
+ros::NodeHandle  nh;
+
+sensor_msgs::Range range_msg;
+std_msgs::String str_msg;
+
+ros::Publisher chatter("chatter", &str_msg);
+ros::Publisher pub_range( "/ultrasound", &range_msg);
+
+char hello_msg[] = "ultrasound unit";
+char frameid[] = "/ultrasound";
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+
+  nh.initNode();
+  nh.advertise(pub_range);
+  nh.advertise(chatter);
+
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.header.frame_id =  frameid;
+  range_msg.field_of_view = 0.1;  // fake
+  range_msg.min_range = 0.0;
+  range_msg.max_range = 6.47;
+ 
+  Serial.begin(57600);
   pinMode(BOARD_LED, OUTPUT);
   
   setUltrasound(TRIG_1, ECHO_1);
@@ -78,21 +105,26 @@ void loop() {
   
   readings[1] = read(TRIG_1, ECHO_1);
   readings[2] = read(TRIG_2, ECHO_2);
-  
   /*
   Serial.write('#');
   serialize(reading_A);
   serialize(reading_B);
-  */
   
   Serial.println(readings[1]);
   Serial.println(readings[2]);
+  */
   
   digitalWrite(BOARD_LED, 1);
-  delay(25);
+  delay(5);
 
   digitalWrite(BOARD_LED, 0);
-  delay(25);
+
+  str_msg.data = hello_msg;
+  chatter.publish( &str_msg );
+  range_msg.range = readings[1] / 1000.0f;
+  range_msg.header.stamp = nh.now();
+  pub_range.publish(&range_msg);
+  nh.spinOnce();
 }
 
 void printRegisters(char* charBuffer /* 8 bytes minimum */) {
